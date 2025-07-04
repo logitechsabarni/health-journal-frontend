3febcca2ea1624219b733979d5512022a439bcf3
Sabarni Guha <sabarni.guha2024@uem.edu.in>
2025-07-04 23:29:11 +0530

    Update and rename install-deps.bat to setup-vercel-install-deps.bat

---
+++ setup-vercel-install-deps.bat
@@ -0,0 +1,22 @@
+@echo off
+echo Navigating to client folder...
+cd client
+
+echo Installing required dependencies: react-chartjs-2 and chart.js ...
+npm install react-chartjs-2 chart.js
+
+echo Going back to root folder...
+cd ..
+
+echo Adding all changes to git...
+git add .
+
+echo Committing the changes...
+git commit -m "Install chart.js and react-chartjs-2 dependencies"
+
+echo Pushing to GitHub...
+git push origin main
+
+echo Done! Now try redeploying on Vercel.
+pause
+
