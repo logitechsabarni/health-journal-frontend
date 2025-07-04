206b366c0d63af30f25a27ce9606fe6f6f0f8cc1
Sabarni Guha <sabarni.guha2024@uem.edu.in>
2025-07-04 22:17:04 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.VITE_API_URL;
+const BASE_URL = process.env.REACT_APP_API_URL;;
 
 export const registerUser = async (userData) => {
   const response = await fetch(`${BASE_URL}/api/users/register`, {
